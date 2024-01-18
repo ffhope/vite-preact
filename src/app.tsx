4ceb79586c6e8ead3cc2c249c00364f31c7fe8f2
fu.fanghao <fufanghao@ayayi.net.cn>
2024-01-18 14:30:06 +0800

    feat: microapp

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,46 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
-import { Button, Input } from 'letd';
+import { useState } from "preact/hooks";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
+import { Button, Input } from "letd";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [app, setApp] = useState("preact");
+  return (
+    <>
+      选择子应用：
+      <select
+        onChange={(e: any) => {
+          setApp(e.target.value);
+        }}
+      >
+        <option value="preact">基座应用Preact</option>
+        <option value="react">子应用React</option>
+        <option value="vue">子应用Vue</option>
+        <option value="let">组件库</option>
+      </select>
+      <div
+        style={{
+          height: "80vh",
+          width: "100vw",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+          overflow: 'hidden'
+        }}
+      >
+        {app === "preact" && <PreactApp />}
+        {app === "react" && <ReactApp />}
+        {app === "vue" && <VueApp />}
+        {app === "let" && <LetApp />}
+      </div>
+    </>
+  );
+}
 
+function PreactApp() {
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -19,15 +53,50 @@ export function App() {
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
-      <Button type="primary" onClick={() => setCount((count) => count + 1)}>
+        <Button type="primary" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
         <p></p>
-        <Input value={count}/>
+        <Input value={count} />
       </div>
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
     </>
-  )
+  );
+}
+
+function ReactApp() {
+  return (
+    <micro-app
+      name="react-app"
+      url="https://vite-react.fufanghao.com/"
+      iframe
+    ></micro-app>
+  );
+}
+
+function VueApp() {
+  return (
+    <micro-app
+      name="vue-app"
+      url="https://vite-vue.fufanghao.com/"
+      iframe
+    ></micro-app>
+  );
+}
+
+function LetApp() {
+  return (
+    <iframe
+      // name="let-app"
+      style={{
+        width: "100%",
+        height: "100vw"
+      }}
+      frameBorder={0}
+      src="https://lit.fufanghao.com/"
+      // iframe
+    ></iframe>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
+import microApp from '@micro-zoe/micro-app'
+
+microApp.start()
 
 render(<App />, document.getElementById('app')!)
